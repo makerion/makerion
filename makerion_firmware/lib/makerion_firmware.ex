915956a450f5a1f886d21cfe07cbc2d4f3021c0c
Tim Mecklem <timothy@mecklem.com>
2019-03-02 14:31:18 -0500

    mix nerves.new

---
+++ makerion_firmware/lib/makerion_firmware.ex
@@ -0,0 +1,18 @@
+defmodule MakerionFirmware do
+  @moduledoc """
+  Documentation for MakerionFirmware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MakerionFirmware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ makerion_firmware/test/makerion_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule MakerionFirmwareTest do
+  use ExUnit.Case
+  doctest MakerionFirmware
+
+  test "greets the world" do
+    assert MakerionFirmware.hello() == :world
+  end
+end
