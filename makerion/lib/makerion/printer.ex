402406c9b0b9e40d5b1065e4345cdb7346a9f099
Tim Mecklem <timothy@mecklem.com>
2019-03-17 18:03:50 -0400

    Missed a status

--- makerion/lib/makerion/printer.ex
+++ makerion/lib/makerion/printer.ex
@@ -18,5 +18,7 @@ defimpl Makerion.Printer, for: Moddity.FakeDriver do
   defp translate_state("STATE_JOB_QUEUED"), do: 'Job Queued'
   defp translate_state("STATE_BUILDING"), do: 'Building'
   defp translate_state("STATE_NET_FAILED"), do: 'Net Failed'
+  defp translate_state("STATE_EXEC_PAUSE_CMD"), do: 'Paused'
+
   defp translate_state(state), do: state
 end
