ffad0a557162d402367abe2eed6fb69f1a1d41cb
Tim Mecklem <timothy@mecklem.com>
2019-04-14 21:50:06 -0400

    Trying check_origin: false in tests ¯\_(ツ)_/¯

--- makerion_web/config/test.exs
+++ makerion_web/config/test.exs
@@ -1,7 +1,3 @@
-# Since configuration is shared in umbrella projects, this file
-# should only configure the :makerion_web application itself
-# and only for organization purposes. All other config goes to
-# the umbrella root.
 use Mix.Config
 
 config :hound,
@@ -10,10 +6,9 @@ config :hound,
 
 config :makerion_web, sql_sandbox: true
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :makerion_web, MakerionWeb.Endpoint,
   http: [port: 4001],
+  check_origin: false,
   server: true
 
 config :logger, level: :warn
