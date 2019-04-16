1d3f7cb156a8fc031d6b42191a0f0cafb7dc10c6
Tim Mecklem <timothy@mecklem.com>
2019-04-15 23:44:06 -0400

    Ignore dialyzer warning for now

--- .dialyzer-ignore.exs
+++ .dialyzer-ignore.exs
@@ -1,5 +1,6 @@
 [
   ~r/__impl__.*does not exist\./,
   ~r/The test pid\(\) == nil can never evaluate to 'true'\./,
-  {"lib/components/printer_status.ex", :pattern_match_cov, 6}
+  {"lib/components/printer_status.ex", :pattern_match_cov, 6},
+  {"lib/makerion_web/templates/printer_status/show.html.leex", :pattern_match, 1}
 ]
