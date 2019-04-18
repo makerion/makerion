4bbd0a54c58696d726432ca57da2c4c22c6c65d0
Tim Mecklem <timothy@mecklem.com>
2019-04-17 23:31:28 -0400

    Build and deploy rpi0 and rpi3 artifacts to GitHub on tagged version (#5)

--- makerion_firmware/config/rpi0.exs
+++ makerion_firmware/config/rpi0.exs
@@ -56,29 +56,6 @@ config :makerion, Makerion.Repo,
   adapter: Sqlite.Ecto2,
   database: Path.expand("/root/repo.sqlite3")
 
-config :makerion_kiosk,
-  nerves_networkinterface: Nerves.NetworkInterface
-
-config :makerion_kiosk, :viewport, %{
-  name: :main_viewport,
-  # default_scene: {Nerves.Scene.Crosshair, nil},
-  default_scene: {MakerionKiosk.Scene.SysInfo, nil},
-  size: {800, 480},
-  opts: [scale: 1.0],
-  drivers: [
-    %{
-      module: Scenic.Driver.Nerves.Rpi
-    },
-    %{
-      module: Scenic.Driver.Nerves.Touch,
-      opts: [
-        device: "FT5406 memory based driver",
-        calibration: {{1, 0, 0}, {1, 0, 0}}
-      ]
-    }
-  ]
-}
-
 config :makerion_web, MakerionWeb.Endpoint,
   url: [host: "localhost"],
   http: [port: 80],
