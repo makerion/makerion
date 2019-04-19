d45a631b71b863d15147d0944474d8824be6b642
Tim Mecklem <timothy@mecklem.com>
2019-04-18 23:00:34 -0400

    Use new scenic push: graph approach

--- makerion_kiosk/lib/components/printer_status.ex
+++ makerion_kiosk/lib/components/printer_status.ex
@@ -10,36 +10,23 @@ defmodule MakerionKiosk.Components.PrinterStatus do
 
   import Scenic.Primitives, only: [{:text, 2}, {:text, 3}, {:group, 3}]
 
-  @target System.get_env("MIX_TARGET") || "host"
-
-  @system_info """
-  MIX_TARGET: #{@target}
-  MIX_ENV: #{Mix.env()}
-  Scenic version: #{Scenic.version()}
-  """
-
   @graph Graph.build(font_size: 22, font: :roboto_mono)
   |> group(
   fn g ->
     g
     |> text("System")
     |> text("IP Address", translate: {10, 20}, id: :ip_address)
-    |> text(@system_info, translate: {0, 40}, font_size: 18, id: :printer_status_text)
+    |> text("", translate: {0, 40}, font_size: 18, id: :printer_status_text)
   end,
   t: {10, 30}
   )
 
   # --------------------------------------------------------
   def init(_, _opts) do
-    graph =
-      @graph
-    # |> Graph.modify(:device_list, &update_opts(&1, hidden: @target == "host"))
-    |> push_graph()
-
     Driver.subscribe()
     Process.send_after(self(), :update_ip, 5_000)
 
-    {:ok, graph}
+    {:ok, @graph, push: @graph}
   end
 
   def verify(data), do: {:ok, data}
@@ -54,10 +41,9 @@ defmodule MakerionKiosk.Components.PrinterStatus do
           graph =
             graph
             |> Graph.modify(:ip_address, &text(&1, "IP Address #{ipv4_address}"))
-            |> push_graph()
 
           Process.send_after(self(), :update_ip, 120_000)
-          {:noreply, graph}
+          {:noreply, graph, push: graph}
 
         _ ->
           Process.send_after(self(), :update_ip, 5_000)
@@ -75,8 +61,7 @@ defmodule MakerionKiosk.Components.PrinterStatus do
     graph =
       graph
       |> Graph.modify(:printer_status_text, &text(&1, status.state_friendly))
-      |> push_graph()
 
-    {:noreply, graph}
+    {:noreply, graph, push: graph}
   end
 end
